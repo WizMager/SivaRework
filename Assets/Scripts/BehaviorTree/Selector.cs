31cfca2e4cb470fa24287e29816f51f65db6b26f
WizMager <wizmager@gmail.com>
2023-09-03 12:17:55 +0300

    -add cinemachine -start work on BTree

---
+++ Assets/Scripts/BehaviorTree/Selector.cs
@@ -0,0 +1,28 @@
+﻿namespace BehaviorTree
+{
+    public class Selector : Node
+    {
+        public override NodeState Evaluate()
+        {
+            foreach (var child in Children)
+            {
+                switch (child.Evaluate())
+                {
+                    case NodeState.Running:
+                        State = NodeState.Running;
+                        return State;
+                    case NodeState.Success:
+                        State = NodeState.Success;
+                        return State;
+                    case NodeState.Failure:
+                        continue;
+                    default:
+                        continue;
+                }
+            }
+
+            State = NodeState.Failure;
+            return State;
+        }
+    }
+}
---
+++ Assets/Scripts/BehaviorTree/Sequence.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace BehaviorTree
+{
+    public class Sequence : Node
+    {
+        public override NodeState Evaluate()
+        {
+            var anyChildIsRunning = false;
+
+            foreach (var child in Children)
+            {
+                switch (child.Evaluate())
+                {
+                    case NodeState.Running:
+                        anyChildIsRunning = true;
+                        continue;
+                    case NodeState.Success:
+                        continue;
+                    case NodeState.Failure:
+                        State = NodeState.Failure;
+                        return State;
+                    default:
+                        State = NodeState.Success;
+                        return State;
+                }
+            }
+
+            State = anyChildIsRunning ? NodeState.Running : NodeState.Success;
+            return State;
+        }
+    }
+}
